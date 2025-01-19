c380c9ec4de91b28fb9c7b8f4ccda08eeb306b4d
mahdi_dev <hadjidjmahdi@gmail.com>
2025-01-19 19:03:54 +0100

    update for black background

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
-
+let h1 = document.getElementById('h1');
 function setcolor(color){
     document.body.style.background = color;
+    if(color === 'black'){
+        h1.style.color = 'white';
+    }
+    else{
+        h1.style.color = 'black';
+    }
 }
 function randomcolor(){
     const red = Math.round(Math.random() * 255);
     const green = Math.round(Math.random() * 255);
     const blue = Math.round(Math.random() * 255);
 
-    const rdcolor = `rgba(${red}, ${green}, ${blue})`;
+    const rdcolor = `rgb(${red}, ${green}, ${blue})`;
     setcolor(rdcolor);
+    
 }
